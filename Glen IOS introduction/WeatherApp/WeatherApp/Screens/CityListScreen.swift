5f6915e51b746c4d3fb2edfcd4982a9cb89eda78
GlenDorner221358 <113541242+GlenDorner221358@users.noreply.github.com>
2023-10-04 16:19:48 +0200

    Week 3 class progress
    uploaded all the files I can

---
+++ Glen IOS introduction/WeatherApp/WeatherApp/Screens/CityListScreen.swift
@@ -0,0 +1,20 @@
+//
+//  CityListScreen.swift
+//  WeatherApp
+//
+//  Created by student on 2023/10/04.
+//
+
+import SwiftUI
+
+struct CityListScreen: View {
+    var body: some View {
+        Text("Hello Cities!")
+    }
+}
+
+struct CityListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        CityListScreen()
+    }
+}
