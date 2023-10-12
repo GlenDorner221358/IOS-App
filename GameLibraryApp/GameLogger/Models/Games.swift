08c49dd519e220a011fae70929d1d81b7c68b33f
GlenDorner221358 <113541242+GlenDorner221358@users.noreply.github.com>
2023-10-12 13:38:57 +0200

    Worked on progress milestone 3
    Added Model and data for 3 games

---
+++ GameLibraryApp/GameLogger/Models/Games.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+//Gamedata blueprint
+
+struct Game: Identifiable {
+    var id = UUID() //unique id for each city that auto generates so dont have to make a new one
+    var title: String
+    //TODO: Add images for each game
+    //var boxart: Image
+    var dev: String
+    var genre: String
+    var description: String
+    var releaseDate: String
+    var platform: String
+    var rating: String
+    var how2play: String
+}
+
+//dummy city from blueprint for previews
+let dummyGame: Game = Game(
+    title: "SSX3",
+    dev: "EA Sports BIG",
+    genre: "Arcade",
+    description: "Extreme snowboarding arcade action, the greatest video game of all time. Excellent soundtrack, fun characters, in depth trick system. Whats not to like?",
+    releaseDate: "20/10/2003",
+    platform: "Playstation 2",
+    rating: "9.4",
+    how2play: "1) Put the disc into the playstation2. 2) Reset the console. 3) Allow game to load up. 4) Select 'Conquer the mountain' for campaign mode or 'single event' to do a single race. 5) Pick a character. 6) Rejoice in God's creation."
+)
---
+++ GameLibraryApp/GameLogger/Screens/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  GameLogger
+//
+//  Created by student on 2023/10/12.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
