e363312f7d9f70a271c6df0b6e0508a34fc5102d
GlenDorner221358 <221358@virtualwindow.co.za>
2023-11-02 21:50:39 +0200

    last minute
    Need to sort out my code so I can use Armands mac fast

--- GameLibraryApp/GameLogger/Screens/HomeScreen.swift
+++ GameLibraryApp/GameLogger/Screens/HomeScreen.swift
@@ -8,37 +8,41 @@
 import SwiftUI
 
 struct HomeScreen: View {
+
+    var Game: Game
+
+    var total = 0
+
     var body: some View {
         ZStack{
             LinearGradient(colors: [.teal, .white], startPoint: .top, endPoint: .bottomTrailing)
                 .ignoresSafeArea(.all)
             
             VStack{
-                HStack{
-                    Image(systemName: "gearshape.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .padding(.leading)
-                    
-                    HStack{
-                        Image(systemName: "magnifyingglass")
-                            .renderingMode(.original)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 40, height: 40)
-                        TextField("Search...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                    }.padding(10).background(.white).cornerRadius(20).frame(width: 100, height: 50)
-                }//END SETTINGS AND SEARCH
+                Image(systemName: "gearshape.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+                    .alignment(.trailing)
+
                 Spacer()
                         
-                Button("New Item") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                ForEach(){ game in 
+                    total = total + 1
                 }
+
+                Text("Total games: " + )
+                    .font(.title)
+                    .bold()
+                    .alignment(leading)
                 
                 Spacer()
-                
+
+                NavigationLink(destination: LibraryScreen()) {
+                    Text("Browse collection")
+                }//END OF NAVLINK
+
                 
             }
         }
